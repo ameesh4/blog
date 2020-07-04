2292025d5fd8cb414d78df42c84effbc26a9ab3d
amishsharma <amishsharma.6936@gmail.com>
2020-07-04 16:00:35 +0545

    Demo

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,13 @@
+import requests
 from django.shortcuts import render
 from django.views import generic
-
+from django.http import HttpResponse
 from .models import POST
+
+def index(request):
+    r = requests.get('http://httpbin.org/status/418')
+    print(r.text)
+    return HttpResponse('<pre>' + r.text + '</pre>')
 
 class PostList(generic.ListView):
     queryset = POST.objects.filter(status=1).order_by('-created_on')
